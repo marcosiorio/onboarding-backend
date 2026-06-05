f2a939b7fe6d0a65ea57a01738a807ac487d7f88
Marcos <marcos-light-it@MacBook-Pro-de-Marcos.local>
2026-06-05 16:33:12 -0300

    feat: add sedeers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use Database\Factories\ClinicFactory;
+use Database\Factories\DoctorFactory;
+use Database\Factories\PatientFactory;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,5 +21,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         UserFactory::new()->createMany(35);
+        PatientFactory::new()->createMany(20);
+
+        $clinics = ClinicFactory::new()->createMany(10);
+        $doctors = DoctorFactory::new()->createMany(15);
+
+        $clinics->each(function ($clinic) use ($doctors) {
+            $clinic->doctors()->attach(
+                $doctors->random(rand(1, 5))->pluck('id')->toArray()
+            );
+        });
     }
 }
